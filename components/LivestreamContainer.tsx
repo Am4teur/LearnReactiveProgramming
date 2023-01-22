3709ce7ceaf5c88353a31aa74d523bc8af184d1b
dcastro <dcastro@tripadvisor.com>
2023-01-22 14:39:07 +0000

    Add (on purpose) wrong observer behavior
    the observer pipe runs twice on the reactive.js
    Why? because I pipe the first one with a map, then the second pipe will execute the first pipe again (getting the random frame again) and so the second pipe can have a different frame than the first one

--- components/LivestreamContainer.tsx
+++ components/LivestreamContainer.tsx
@@ -1,62 +1,104 @@
+import { obsFunction$ } from "@models/reactive";
 import { motion } from "framer-motion";
+import NextImage from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { fromEvent } from "rxjs";
+import { fromEvent, map } from "rxjs";
 
 const LivestreamContainer = () => {
   const buttonEl = useRef<any>(null);
-  const [arr, setArr] = useState<any[]>([]);
+  const count = useRef(0);
+  const [frames, setFrames] = useState<string[]>([]);
+  const [frames2, setFrames2] = useState<string[]>([]);
 
   useEffect(() => {
-    const obs$ = fromEvent(buttonEl.current, "click").pipe();
+    const obs$ = fromEvent(buttonEl.current, "click").pipe(map(getRandomFrame));
 
-    const subscriber = obs$.subscribe((clickety: any) =>
-      setArr((prev) => [...prev, clickety.timeStamp])
-    );
+    const interactiveObserver = obsFunction$(obs$);
 
-    return () => subscriber.unsubscribe();
+    const subscriber = obs$.subscribe((e: string) => {
+      console.log("1sub", e);
+
+      setFrames((prev) => [...prev, e]);
+    });
+
+    const subscriberInteractive = interactiveObserver.subscribe((e: string) => {
+      console.log("2sub", e);
+
+      setFrames2((prev) => [...prev, e]);
+    });
+
+    return () => {
+      subscriber.unsubscribe();
+      subscriberInteractive.unsubscribe();
+    };
   }, []);
 
-  const addRandom = () => {
-    // obs$.next(Math.random());
+  const getRandomFrame = (): string => {
+    count.current = count.current + 1;
+    console.log(count.current);
+
+    return ["react-icon", "rxjs-icon"][count.current % 2];
   };
+
+  const getFrameSrc = (frame: string): string =>
+    "/livestream/frames/" + frame + ".png";
+
+  const velo = 0.5;
 
   return (
     <>
-      <div className="h-full w-full flex flex-col items-center justify-center">
-        <motion.div
-          className="absolute left-0 right-0 top-0 bottom-0 m-auto border-solid border-white border-2 rounded-md w-64 h-64 bg-blue-400"
-          initial={{ y: "-200%" }}
-          animate={{ y: "0" }}
-          transition={{ delay: 4, duration: 2, ease: "linear" }}
-        ></motion.div>
-        <motion.div
-          className="absolute left-0 right-0 top-0 bottom-0 m-auto border-solid border-white border-2 rounded-md w-64 h-64 bg-blue-400"
-          initial={{ y: "-200%" }}
-          animate={{ y: "0" }}
-          transition={{ delay: 2, duration: 2, ease: "linear" }}
-        ></motion.div>
-        <motion.div
-          className="absolute left-0 right-0 top-0 bottom-0 m-auto border-solid border-white border-2 rounded-md w-64 h-64 bg-blue-400"
-          initial={{ y: "-200%" }}
-          animate={{ y: "0" }}
-          transition={{ duration: 2, ease: "linear" }}
-        ></motion.div>
-        <div className="border-solid border-white border-2  rounded-md w-64 h-64"></div>
+      <div className="flex justify-center h-full w-full py-8 bg-slate-400">
         <button
           ref={buttonEl}
-          className="absolute left-5 top-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded shadow"
-          onClick={addRandom}
+          className="absolute left-5 top-5
+          bg-[#60dafa] hover:bg-blue-500 border border-[#60dafa] hover:border-blue-500 rounded shadow
+          font-bold py-2 px-4"
         >
           Send Event
         </button>
-        {arr.map((val) => (
+
+        <div className="generator self-start absolute border-solid border-white border-2 rounded-md w-32 h-32"></div>
+
+        <div
+          className="pipe self-center absolute border-solid border-white border rounded-md w-32 h-64
+        bg-gray-600"
+        ></div>
+
+        <div className="screen self-end absolute border-solid border-white border-2 rounded-md w-32 h-32"></div>
+
+        {frames.map((frame: any, i: number) => (
           <motion.div
-            key={val}
-            className="absolute left-0 right-0 top-0 bottom-0 m-auto border-solid border-white border-2 rounded-md w-64 h-64 bg-blue-400"
-            initial={{ y: "-200%" }}
+            key={i}
+            className="self-center absolute flex items-center justify-center
+            border-solid border-white border-2 rounded-md w-16 h-16 bg-blue-400"
+            initial={{ y: "-500%" }} // hardcoded values
             animate={{ y: "0" }}
-            transition={{ duration: 2, ease: "linear" }}
-          ></motion.div>
+            transition={{ duration: velo, ease: "linear" }}
+          >
+            <NextImage
+              src={getFrameSrc(frame)}
+              alt="Frame"
+              width={640}
+              height={640}
+            />
+          </motion.div>
+        ))}
+        {frames2.map((frame: any, i: number) => (
+          <motion.div
+            key={i}
+            className="self-center absolute flex items-center justify-center
+            border-solid border-black border-2 rounded-md w-16 h-16 bg-blue-400"
+            initial={{ y: "0" }}
+            animate={{ y: "550%" }} // hardcoded values
+            transition={{ delay: velo, duration: velo, ease: "linear" }}
+          >
+            <NextImage
+              src={getFrameSrc(frame)}
+              alt="Frame"
+              width={640}
+              height={640}
+            />
+          </motion.div>
         ))}
       </div>
     </>
--- models/reactive.ts
+++ models/reactive.ts
@@ -1,10 +1,19 @@
-import { from, map, tap } from "rxjs";
+import { filter, from, tap } from "rxjs";
 
 const framesStream1 = ["frame1", "frame2", "frame3"];
 // const framesStream2 = ["wrong-frame1", "frame2", "frame3"];
 // const framesStream3 = ["frame1", "frame2", "frame3"];
 
 const obs$ = from(["frame1", "frame2", "frame3"]).pipe(
-  tap((e: any) => {}),
-  map((e: any) => console.log(e))
+  filter((e: any) => e === "react-icon")
+  // tap((e: any) => {}),
+  // map((e: any) => console.log(e))
 );
+
+export const obsFunction$ = (observer: any) =>
+  observer.pipe(
+    tap(console.log),
+    filter((e: string) => e === "react-icon")
+    // tap((e: any) => {}),
+    // map((e: any) => console.log(e))
+  );
